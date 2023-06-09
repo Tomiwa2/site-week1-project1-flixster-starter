5023bba3b48561f6d7c210b9463d8cd0b8d1e949
Iretomiwa Idowu <iidowu@iidowu-ltm3jmq.internal.salesforce.com>
2023-06-09 16:03:25 -0700

    rendered cards, made search dynamic

--- script.js
+++ script.js
@@ -1,35 +1,115 @@
 const apiKey = "0691c6593217008dabe52607e2449d01"
 
 let movieContainer = document.createElement('div');
-let searchForm = document.getElementById('search-button')
+movieContainer.classList.add("movie-container");
+let searchForm= document.getElementById('search-form')
 let searchInput = document.getElementById('search-input')
+let loadMore = document.getElementById("load-more")
 document.body.appendChild(movieContainer);
-
-fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=0691c6593217008dabe52607e2449d01")
+ 
+fetch("https://api.themoviedb.org/3/movie/now_playing?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=0691c6593217008dabe52607e2449d01")
     .then(res => res.json())
     .then(data => {
         data.results.forEach((movie) => {
-            generateCard(movie)
+           generateCards(movie)
+            // console.log(data)
         })
     })
 
-function generateCard(movieObject) {
-    let star = document.createElement('span');
-    star.classList.add('star');
-    let starContent = document.createTextNode(':star:️');
-    star.appendChild(starContent);
-    //create rating
-    let rating = document.createElement('span');
-    rating.classList.add('rating');
-    let ratingContent = document.createTextNode(movieObject.vote_average);
-    rating.appendChild(ratingContent);
-    //create average container
-    let averageContainer = document.createElement('div');
-    averageContainer.classList.add('averageContainer');
-    averageContainer.appendChild(star);
-    averageContainer.appendChild(rating);
-    document.body.appendChild(averageContainer);
-}
+    function generateCards(movieObject){
+        let star = document.createElement('span');
+        star.classList.add('star');
+        let starContent = document.createTextNode('⭐️ ');
+        star.appendChild(starContent);
+        // document.body.appendChild(star);
+        //create rating
+        let rating = document.createElement('span');
+        let ratingContent = document.createTextNode(movieObject.vote_average);
+        star.classList.add('rating');
+        star.appendChild(ratingContent);
+        // document.body.appendChild(rating);
+        //create average
+        let averageContainer = document.createElement('div');
+        star.classList.add('average');
+        averageContainer.appendChild(star);
+        averageContainer.appendChild(rating);
+        // document.body.appendChild(averageContainer);
+        let image = document.createElement('img');
+        console.log(movieObject)
+        image.src = "https://image.tmdb.org/t/p/w342/"+movieObject.poster_path;
+        // document.body.insertBefore(image, averageContainer);
+        //create name section
+        let name = document.createElement('div');
+        name.classList.add('name');
+        name.innerText = movieObject.original_title;
+        // document.body.insertBefore(name, averageContainer.nextSibling);
+        //create movie section
+        let movie = document.createElement('section');
+        name.classList.add('movie');
+        movie.appendChild(image);
+        movie.appendChild(averageContainer);
+        movie.appendChild(name);
+        movieContainer.appendChild(movie)
+        //document.body.appendChild(movie);
+       // return movie;
+    }
+
+    
+    searchForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let searchValue = searchInput.value 
+        // console.log("search value", searchValue)
+        movieContainer.innerHTML = ''
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchValue}`)
+    .then(res => res.json())
+    .then(data => {
+        data.results.forEach((movie) => {
+           generateCards(movie)
+            // console.log(data)
+        })
+    })
+    })
+     
+    let pageNum = 2
+    let result = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}` + "&page=" 
+    console.log(result+pageNum)
+    // console.log(loadMore);
+
+    loadMore.addEventListener("click", () => {
+    
+        // console.log("search value", searchValue)
+        // console.log(result+pageNum)
+        fetch(result+pageNum)
+    .then(res => res.json())
+    .then(data => {
+        data.results.forEach((movie) => {
+           generateCards(movie)
+            // console.log(data)
+            // pageNum++;
+        })
+    })
+    pageNum++;
+    console.log(pageNum);
+
+})
+    
+     
+
+    
+    // searchButton.addEventListener("submit", handleSearch)
+//     const  movieContainer = document.querySelector('#movie-container')
+// const loadMoreButton = document.querySelector('#load-more-button')
+// function display (page){
+// fetch( `https://api.themoviedb.org/3/discover/movie?api_key=57b58a649f61e3a9ea0cf603c932ffec&page=${page} `)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     for (let i = 0; i < data.results.length; i++) {
+//       movieContainer.appendChild(generateCards(data.results[i]));
+//     }
+//   });
+// }
+
 
 //search form event listener
 
